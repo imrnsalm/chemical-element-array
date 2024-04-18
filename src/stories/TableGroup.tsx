12782f7507bcfd000940d08ac18b0572750ac65d
i8abyte <imransalim08@gmail.com>
2024-04-18 17:28:48 +1200

    Change the name of this project

---
+++ src/stories/TableGroup.tsx
@@ -0,0 +1,31 @@
+// import { ChemicalElement } from "./ChemicalElement";
+
+
+// interface TableGroupProps {
+//   groupNumber?: number,
+
+//   nameByElement?: string,
+
+//   numberOfElements?: number,
+
+//   elements?: typeof ChemicalElement[]
+// }
+
+// export const TableGroup = ({
+//   groupNumber = 1,
+//   nameByElement = 'lithium group',
+//   numberOfElements = 7,
+//   ...props
+// }: TableGroupProps) => {
+//   return (
+//     <div
+//       className="storybook-table-group"
+//       {...props}
+//     >
+//       <div id="group-number" hidden>{groupNumber}</div>
+//       <div id="name-by-element" hidden>{nameByElement}</div>
+//       <div id="number-of-elements" hidden>{numberOfElements}</div>
+//       {elements}
+//     </div>
+//   );
+// };
